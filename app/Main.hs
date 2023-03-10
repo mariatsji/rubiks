3bebf7298c039f4d71088b6c3e86264964e75fe6
Sjur Millidahl <smillida@cisco.com>
2023-03-10 10:09:14 +0100

    initial drawing

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,28 @@
+{-# language TemplateHaskell #-}
 module Main where
 
 import Cube
 
 import Brick
+import Brick.AttrMap
 import Brick.Widgets.Core ((<=>), (<+>), hBox, vBox)
+import Graphics.Vty.Attributes (defAttr)
+import Lens.Micro.Mtl
+import Lens.Micro.TH
 import Relude
 
-ui :: Widget ()
-ui = vBox
+-- must be free in n?
+data MyAppState n = MyAppState Cube
+
+makeLenses ''MyAppState
+
+drawUi :: MyAppState () -> [Widget ()]
+drawUi _ = [ vBox
     [ hBox [ dummy, headWidget, dummy ],
       hBox [ leftWidget, torsoWidget, rightWidget ],
       hBox [ dummy, legsWidget, dummy ],
       hBox [ dummy, feetWidget, dummy ]
-    ] -- str "Rubiks Cube"
+    ]]
     where
         fillNine c = hLimit 3 (vLimit 3 ( fill c ))
         dummy = fillNine ' '
@@ -22,9 +32,22 @@ ui = vBox
         rightWidget = fillNine 'B'
         legsWidget = fillNine 'O'
         feetWidget = fillNine 'W'
-        
+
+appEvent :: BrickEvent () e -> EventM () (MyAppState ()) ()
+appEvent = const $ pure ()
+
+app :: App (MyAppState ()) e ()
+app = App {
+        appDraw = drawUi,
+        appChooseCursor = \_ _ ->  Nothing,
+        appHandleEvent = appEvent,
+        appStartEvent = pure (),
+        appAttrMap = \_ -> attrMap defAttr []
+    }
+
+initState :: MyAppState ()
+initState = MyAppState initCube
 
 main :: IO ()
 main = do
-    let cube = initCube
-    simpleMain ui
+    void $ defaultMain app initState
