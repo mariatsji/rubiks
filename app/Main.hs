50338d4a36b277fd57e4e32aae88a96cec3201cb
Sjur Millidahl <smillida@cisco.com>
2023-03-10 13:27:32 +0100

    COLORS

--- app/Main.hs
+++ app/Main.hs
@@ -1,53 +1,85 @@
-{-# language TemplateHaskell #-}
 module Main where
 
 import Cube
 
 import Brick
-import Brick.AttrMap
-import Brick.Widgets.Core ((<=>), (<+>), hBox, vBox)
+import Graphics.Vty (Attr)
 import Graphics.Vty.Attributes (defAttr)
-import Lens.Micro.Mtl
-import Lens.Micro.TH
+import Graphics.Vty.Attributes.Color (
+    blue,
+    green,
+    red,
+    rgbColor,
+    white,
+    yellow,
+ )
 import Relude
 
--- must be free in n?
-data MyAppState n = MyAppState Cube
+drawUi :: Cube -> [Widget ()]
+drawUi cube =
+    [ vBox
+        [ hBox [dummy, draw $ _head cube, dummy]
+        , hBox [draw $ _leftArm cube, draw $ _torso cube, draw $ _rightArm cube]
+        , hBox [dummy, draw $ _legs cube, dummy]
+        , hBox [dummy, draw $ _feet cube, dummy]
+        ]
+    ]
+  where
+    fillNine c = hLimit 3 (vLimit 3 (fill c))
+    dummy = fillNine ' '
+    headWidget = fillNine 'R'
+    leftWidget = fillNine 'G'
+    torsoWidget = fillNine 'Y'
+    rightWidget = fillNine 'B'
+    legsWidget = fillNine 'O'
+    feetWidget = fillNine 'W'
 
-makeLenses ''MyAppState
+draw :: Face -> Widget ()
+draw ((a, b, c), (d, e, f), (g, h, i)) =
+    ( withAttr (color a) (str " ")
+        <+> withAttr (color b) (str " ")
+        <+> withAttr (color c) (str " ")
+    )
+        <=> ( withAttr (color d) (str " ")
+                <+> withAttr (color e) (str " ")
+                <+> withAttr (color f) (str " ")
+            )
+        <=> ( withAttr (color g) (str " ")
+                <+> withAttr (color h) (str " ")
+                <+> withAttr (color i) (str " ")
+            )
+  where
+    color :: Sticker -> AttrName
+    color = attrName . \case
+        Red -> "red"
+        Green -> "green"
+        Blue -> "blue"
+        Yellow -> "yellow"
+        White -> "white"
+        Orange -> "orange"
 
-drawUi :: MyAppState () -> [Widget ()]
-drawUi _ = [ vBox
-    [ hBox [ dummy, headWidget, dummy ],
-      hBox [ leftWidget, torsoWidget, rightWidget ],
-      hBox [ dummy, legsWidget, dummy ],
-      hBox [ dummy, feetWidget, dummy ]
-    ]]
-    where
-        fillNine c = hLimit 3 (vLimit 3 ( fill c ))
-        dummy = fillNine ' '
-        headWidget = fillNine 'R'
-        leftWidget = fillNine 'G'
-        torsoWidget = fillNine 'Y'
-        rightWidget = fillNine 'B'
-        legsWidget = fillNine 'O'
-        feetWidget = fillNine 'W'
+appEvent :: BrickEvent () e -> EventM () Cube ()
+appEvent = const halt
 
-appEvent :: BrickEvent () e -> EventM () (MyAppState ()) ()
-appEvent = const $ halt
-
-app :: App (MyAppState ()) e ()
-app = App {
-        appDraw = drawUi,
-        appChooseCursor = \_ _ ->  Nothing,
-        appHandleEvent = appEvent,
-        appStartEvent = pure (),
-        appAttrMap = \_ -> attrMap defAttr []
-    }
-
-initState :: MyAppState ()
-initState = MyAppState initCube
+app :: App Cube e ()
+app =
+    App
+        { appDraw = drawUi
+        , appChooseCursor = \_ _ -> Nothing
+        , appHandleEvent = appEvent
+        , appStartEvent = pure ()
+        , appAttrMap = \_ ->
+            attrMap
+                defAttr
+                [ (attrName "blue", bg blue)
+                , (attrName "red", bg red)
+                , (attrName "green", bg green)
+                , (attrName "white", bg white)
+                , (attrName "yellow", bg yellow)
+                , (attrName "orange", bg $ rgbColor 255 165 0)
+                ]
+        }
 
 main :: IO ()
 main = do
-    void $ defaultMain app initState
+    void $ defaultMain app initCube
--- src/Cube.hs
+++ src/Cube.hs
@@ -1,39 +1,82 @@
-module Cube(Color(..), Face(..), Cube(..), initCube) where
+{-# LANGUAGE TemplateHaskell #-}
 
-import Relude
+module Cube (Sticker (..), Face, Cube (..), initCube) where
 
-data Color = Yellow | Red | Green | White | Orange | Blue
+import Lens.Micro
+import Lens.Micro.TH
+import Relude hiding (head)
+
+data Sticker = Yellow | Red | Green | White | Orange | Blue
     deriving stock (Eq)
 
-data Face = Face
-    { top :: (Color, Color, Color)
-    , mid :: (Color, Color, Color)
-    , bot :: (Color, Color, Color)
-    }
+data Place = Head | Torso | LeftArm | RightArm | Legs | Feet
     deriving stock (Eq)
 
 data Cube = Cube
-    { head :: Face
-    , torso :: Face
-    , right :: Face
-    , left :: Face
-    , legs :: Face
-    , feet :: Face
-    } deriving stock (Eq)
+    { _head :: Face
+    , _torso :: Face
+    , _rightArm :: Face
+    , _leftArm :: Face
+    , _legs :: Face
+    , _feet :: Face
+    }
+    deriving stock (Eq)
 
-allSame :: Color -> Face
-allSame c = Face {
-    top = (c,c,c),
-    mid = (c,c,c),
-    bot = (c,c,c)
-}
+type Face =
+    ( (Sticker, Sticker, Sticker)
+    , (Sticker, Sticker, Sticker)
+    , (Sticker, Sticker, Sticker)
+    )
+
+makeLenses ''Cube
+
+data Direction = Clockwise | AntiClockwise
+    deriving stock (Eq)
+
+data Move = Move Place Direction
+    deriving stock (Eq)
+
+allSame :: Sticker -> Face
+allSame c =
+        ( (c, c, c)
+        , (c, c, c)
+        , (c, c, c)
+        )
 
 initCube :: Cube
-initCube = Cube
-    { head = allSame Red
-    , torso = allSame Yellow
-    , right = allSame Blue
-    , left = allSame Green
-    , legs = allSame Orange
-    , feet = allSame White
-    }
+initCube =
+    Cube
+        { _head = allSame Red
+        , _torso = allSame Yellow
+        , _rightArm = allSame Blue
+        , _leftArm = allSame Green
+        , _legs = allSame Orange
+        , _feet = allSame White
+        }
+
+move :: Move -> Cube -> Cube
+move (Move place dir) cube = case (place, dir) of
+    (Torso, Clockwise) ->
+        cube
+            & head . _3 . _1 .~ (cube ^. leftArm . _3 . _3)
+            & head . _3 . _2 .~ (cube ^. leftArm . _2 . _3)
+            & head . _3 . _3 .~ (cube ^. leftArm . _1 . _3)
+            & leftArm . _1 . _3 .~ (cube ^. legs . _1 . _1)
+            & leftArm . _2 . _3 .~ (cube ^. legs . _1 . _2)
+            & leftArm . _3 . _3 .~ (cube ^. legs . _1 . _3)
+            & rightArm . _1 . _1 .~ (cube ^. head . _3 . _1)
+            & rightArm . _2 . _1 .~ (cube ^. head . _3 . _2)
+            & rightArm . _3 . _1 .~ (cube ^. head . _3 . _3)
+            & legs . _1 . _1 .~ (cube ^. rightArm . _1 . _1)
+            & legs . _1 . _2 .~ (cube ^. rightArm . _2 . _1)
+            & legs . _1 . _3 .~ (cube ^. rightArm . _3 . _1)
+            & torso . _1 . _1 .~ (cube ^. torso . _3 . _1)
+            & torso . _1 . _2 .~ (cube ^. torso . _2 . _1)
+            & torso . _1 . _3 .~ (cube ^. torso . _1 . _1)
+            & torso . _2 . _1 .~ (cube ^. torso . _3 . _2)
+            & torso . _2 . _2 .~ (cube ^. torso . _2 . _2)
+            & torso . _2 . _3 .~ (cube ^. torso . _1 . _2)
+            & torso . _3 . _1 .~ (cube ^. torso . _3 . _3)
+            & torso . _3 . _2 .~ (cube ^. torso . _2 . _3)
+            & torso . _3 . _3 .~ (cube ^. torso . _1 . _3)
+    _ -> cube
