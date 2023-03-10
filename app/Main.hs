d196ae1c764e15cd501b48a57d13de0ba02066dd
Sjur Millidahl <smillida@cisco.com>
2023-03-10 09:25:50 +0100

    box layout

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,26 @@ module Main where
 import Cube
 
 import Brick
+import Brick.Widgets.Core ((<=>), (<+>), hBox, vBox)
 import Relude
 
 ui :: Widget ()
-ui = str "Rubiks Cube"
+ui = vBox
+    [ hBox [ dummy, headWidget, dummy ],
+      hBox [ leftWidget, torsoWidget, rightWidget ],
+      hBox [ dummy, legsWidget, dummy ],
+      hBox [ dummy, feetWidget, dummy ]
+    ] -- str "Rubiks Cube"
+    where
+        fillNine c = hLimit 3 (vLimit 3 ( fill c ))
+        dummy = fillNine ' '
+        headWidget = fillNine 'R'
+        leftWidget = fillNine 'G'
+        torsoWidget = fillNine 'Y'
+        rightWidget = fillNine 'B'
+        legsWidget = fillNine 'O'
+        feetWidget = fillNine 'W'
+        
 
 main :: IO ()
 main = do
